bc2eb8ba35dad646e1173f14b6ce6fcdeae20607
cimilcharly <cimilcharly@btech.christuniversity.in>
2026-05-19 16:24:05 +0530

    Remove datadump to protect live data

--- build.sh
+++ build.sh
@@ -6,6 +6,3 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-# Load initial data automatically
-python manage.py loaddata datadump.json || true
