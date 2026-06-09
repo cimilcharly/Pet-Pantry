4df5c8a409b901879c381eee986612108a9263e8
cimilcharly <cimilcharly@btech.christuniversity.in>
2026-06-09 15:16:03 +0530

    fix: serve media files in production and track MEDIA folder in git

--- Pantryproject/urls.py
+++ Pantryproject/urls.py
@@ -87,5 +87,11 @@ urlpatterns = [
     path('delivered_update/<int:d>', views.delivered_update),
 
 ]
-if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+from django.views.static import serve
+from django.urls import re_path
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
